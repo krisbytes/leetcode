093a9ce0c6e48b39c225264cf43e9a44d26e857e
krisbytes <112455928+krisbytes@users.noreply.github.com>
2025-08-26 14:36:43 +0800

    Add files via upload

---
+++ tests/test_add_integers.py
@@ -0,0 +1,15 @@
+import unittest
+from solutions.add_integers import Solution
+
+class TestAddIntegers(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_add(self):
+        self.assertEqual(self.solution.add(1, 2), 3)
+        self.assertEqual(self.solution.add(-1, 1), 0)
+        self.assertEqual(self.solution.add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
